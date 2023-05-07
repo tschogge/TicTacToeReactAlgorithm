45c94cfd6218c61a9995096686166f2c81f9099f
Tschogge <gioele.petrillo@gmail.com>
2023-05-07 13:54:24 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {calculateNewValue} from "@testing-library/user-event/dist/utils";
 
 function Square(props) {
     return (
